6d3f88e39ed2f1cbfbb2002c874b14320af87e67
leeyehoon1208 <pmhherolyh1234@gmail.com>
2023-04-10 11:10:10 +0900

    update README.md
    implement homework#2 (simple-calculator.py)

---
+++ simple-calculator.py
@@ -0,0 +1,31 @@
+def arithmetic_ops(op):
+    num = int(input("input 1st number:"))
+    num = int(input("input 2nd number:"))
+    return num1, num2, op(num1, num2)
+
+def add(x,y): return x+y
+def sub(x,y): return x-y
+
+while True:
+    oop = input("input operation: ")
+    if op == "end":
+        break
+    elif op == "+":
+        num1, num2, ret = arithmetic_ops((add))
+    elif op == "-":
+        num1, num2, ret = arithmetic_ops((sub))
+    elif op == "*":
+        num1, num2, ret = arithmetic_ops((lambda x,y:x*y))
+    elif op == "/":
+        num1, num2, ret = arithmetic_ops((lambda x,y:x/y))
+    elif op == "%":
+        num1, num2, ret = arithmetic_ops((lambda x,y:x%y))
+    else:
+        print("Invalid operation")
+        continue
+    print(f"{num}{op}{num2} = {ret}")
+    
+    print("Exit program")
+    
+        
+ 
